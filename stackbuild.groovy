799839cd28e1bd7b6c429b6fdee27e3fe5315df7
Joshua Hoblitt <josh@hoblitt.com>
2015-10-28 08:38:23 -0700

    fix typo

--- stackbuild.groovy
+++ stackbuild.groovy
@@ -63,7 +63,7 @@ node('vagrant') {
   def date = new java.util.Date()
   def dateStamp = dateFormat.format(date)
 
-  def objectKey = "newinstall.sh-$BULID_ID-$BOX-$dateStamp.tar.gz"
+  def objectKey = "newinstall.sh-$BUILD_ID-$BOX-$dateStamp.tar.gz"
   echo "objectKey: $objectKey"
 
   def S3_URL = presignUrl(
